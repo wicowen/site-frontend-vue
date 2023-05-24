e3166aae3486bf6f1caaf6e0d680f3f399edd793
wicowen <wico0628@gmail.com>
2023-05-24 17:47:54 +0800

    feat: update vue router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,55 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import IndexView from '../views/IndexView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
+    // langind page
+    {
+      path: "/",
+      name: "main-index",
+      component: IndexView,
+    },
+    {
+      path: "/login",
+      name: "main-login",
+      component: LoginView,
+    },
+
+    // public page with layout
     {
       path: "/",
       component: () => import("../layout/public.vue"),
       children: [
-        // {
-        //   path: "",
-        //   name: "public-index",
-        //   component: () => import("../views/public/index.vue"),
-        // },
         {
-          path: "login",
-          name: "public-login",
-          component: () => import("../views/public/login.vue"),
+          path: "about",
+          name: "public-about",
+          component: () => import("../views/public/AboutView.vue"),
         },
       ],
     },
 
-    // {
-    //   path: "/manage",
-    //   component: () => import("../layout/manage.vue"),
-    //   children: [
-    //     {
-    //       path: "member",
-    //       name: "manage-member",
-    //       component: () => import("../views/manage/member.vue"),
-    //     },
-    //   ],
-    // },
+    // manage page with layout
+    {
+      path: "/manage",
+      component: () => import("../layout/manage.vue"),
+      children: [
+        {
+          path: "member",
+          name: "manage-member",
+          component: () => import("../views/manage/MemberView.vue"),
+        },
+      ],
+    },
 
+    // others
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("../views/NotFoundView.vue"),
+    },
 
     // Original path
     // {
